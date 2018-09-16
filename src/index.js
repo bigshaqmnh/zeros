5ae614201c5acc1f85ae4b513abd8dbf411548e3
Michael <kotikov.misha@gmail.com>
2018-09-16 13:13:13 +0300

    factorial done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let countZeros = 0;
+  
+  for (let i = 5; i <= number; i += 5) {
+    for (let j = 1; i >= Math.pow(5, j); ++j) {
+      if (i % Math.pow(5, j) === 0) {
+        countZeros++;
+      }
+    }
+  }
+
+  return countZeros;
 }
